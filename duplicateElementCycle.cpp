40cb86f3744c64c345b691d5bb89c4ed4b2ccdd2
amey1512 <amey1512@gmail.com>
2020-08-05 01:45:32 +0530

    duplicate element by Floyds cycle

--- duplicateElementCycle.cpp
+++ duplicateElementCycle.cpp
@@ -2,15 +2,33 @@
 using namespace std;
 
 int main(){
-  vector<int> vp{3,10,4,6,3,5,7,8,9,1,9};
+  vector<int> vp{3,10,4,6,2,5,7,8,5,1,9};
 
-  unordered_map<int, int> mp;
-  for(int i=0;i<vp.size();i++){
-    //cout<<mp[i]<<endl;
-    if(mp[vp[i]]!=0){
-      cout<<vp[i]<<endl;
-    }
-    else mp[vp[i]] = 1;
+  /* Basic Hashing */
+  // unordered_map<int, int> mp;
+  // for(int i=0;i<vp.size();i++){
+  //   //cout<<mp[i]<<endl;
+  //   if(mp[vp[i]]!=0){
+  //     cout<<vp[i]<<endl;
+  //   }
+  //   else mp[vp[i]] = 1;
+  // }
+
+
+  /* LinkedList Cycle */
+  int fast = vp[0];
+  int slow = vp[0];
+
+  do{
+    slow = vp[slow];
+    fast = vp[vp[fast]];
+  }while(slow != fast);
+
+  fast = vp[0];
+  while(slow != fast){
+    slow = vp[slow];
+    fast = vp[fast];
   }
-  return 0;
+
+  cout<<slow<<endl;
 }
