aa0ccc739992540a92c4c6c006f002ad7b10b1da
amey1512 <amey1512@gmail.com>
2020-08-30 02:45:09 +0530

    hands on file of java

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+class MyClass {
+  public static void main(String args[]){
+    int intA = 9;
+    double dblA = intA;
+    double dblB = 9.5;
+    // int intB  = dblB;
+    System.out.println(intA);
+    System.out.println(dblA);
+    // System.out.println(intB);
+    System.out.println(dblB);
+  }
+}
